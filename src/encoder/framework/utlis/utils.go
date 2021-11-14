0b46b8f75a854782227b737a5eb8007119e1e6f6
gemirson <gemirson.silva@gmail.com>
2021-11-14 20:34:12 -0300

    feature: add utils

---
+++ src/encoder/framework/utlis/utils.go
@@ -0,0 +1,13 @@
+package utlis
+
+import "encoding/json"
+
+func IsJson(s string) error {
+	var js struct{}
+
+	if err := json.Unmarshal([]byte(s), &js); err != nil {
+		return err
+	}
+	return nil
+
+}
